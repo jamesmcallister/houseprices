f1446db70dcd98cb2197d21295ab06505b949c1d
James McAllister <jrfmcallister@gmail.com>
2017-05-10 10:23:31 +0100

    renamed

--- app/main/test/importFileToFakeFunction.test.js
+++ app/main/test/importFileToFakeFunction.test.js
@@ -2,16 +2,18 @@ import importFileBuilder from '../src/csv/importFileBuilder.js'
 import { expect } from 'chai'
 import sinon, { spy } from 'sinon'
 import config from '../src/config/config.js'
-// 15505038372820
 
 describe('Read file form disk', () => {
   it('can read csv file', () => {
     const fakeFileImportFucntion = sinon.stub().returns(Promise.resolve([]))
 
-    const fakeFileImportClient = {
-      fromFile: fakeFileImportFucntion
-    }
-    return importFileBuilder(fakeFileImportClient)(config.importDataPath).then(() => {
+    const fakeFileImportClient = fakeOptions => ({ fromFile: fakeFileImportFucntion })
+
+    const fakeOptions = x => ({
+      noheader: true
+    })
+
+    return importFileBuilder(fakeFileImportClient)(fakeOptions)(config.importDataPath).then(() => {
       expect(fakeFileImportFucntion.called).true
       expect(fakeFileImportFucntion.calledWith(config.importDataPath)).true
     })
