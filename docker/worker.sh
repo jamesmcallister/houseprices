594ed44a3889b0b0ace7b04c868e2e584ccfc143
Mark <mark.alan.richards@gmail.com>
2017-05-21 23:55:37 +0100

    ignore

---
+++ docker/worker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update
+apt-get install wget git curl build-essential -y 
+apt-get autoclean
+apt-get autoremove
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash 
