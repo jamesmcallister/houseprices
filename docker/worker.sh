2ae05a5224009e65a8dda498ddbb313baf8ce361
Mark <mark.alan.richards@gmail.com>
2017-05-27 21:58:45 +0100

    update bash

--- docker/worker.sh
+++ docker/worker.sh
@@ -4,9 +4,5 @@ apt-get install wget git curl build-essential -y
 apt-get autoclean
 apt-get autoremove
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-
-cat <<EOF >> ~/.bashrc
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-EOF
 cat ~/.bashrc
+chmod a+x ~/.bashrc
